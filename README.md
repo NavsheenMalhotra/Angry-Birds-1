40c13d90cec5659d577f4cd2ed8ffdb22625b199
Navsheen Kaur Malhotra <preet22622@gmail.com>
2020-09-28 18:32:38 +0100

    hii

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # angryBirdsStage2
 Angry Birds stage 2 with Class Inheritance and Images
+Angry Birds is a very famous game and i love it.
--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var object;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +16,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    object=new log(200, 500, 30, PI/2);
+
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -48,6 +50,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    object.display()
 
     box3.display();
     box4.display();
